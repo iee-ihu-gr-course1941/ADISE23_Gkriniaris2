b53f4562e8de089f4730d834d30d3374f6e32c12
xristoskap <basterblak@gmail.com>
2024-01-03 18:02:50 +0200

    php update

--- Gkriniaris/www/BdGr.php
+++ Gkriniaris/www/BdGr.php
@@ -1,21 +1,27 @@
 <?php
-print "HERE!!!!!!!!";
+
+require_once  "../lib/dbconnect.php";
+require_once  "../lib/board.php";
+
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/',trim($_SERVER['PATH_INFO'],'/'));
 // $request - explode('/',trim($_SERVER['SCRIPT_NAME'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
 
-print_r($_SERVER);
+
+print_r($request);
 exit;
+
+
 switch ($r=array_shift($request)){
     case 'board' :
             switch ($b=array_shift($request)) {
                 case '': exit; break;
                 case null : handle_board($method);break;
-                case 'piece': handle_piece($method, $request[0], $request[1], $input);
+                case 'piece':// handle_piece($method, $request[0], $request[1], $input);
                     break;
-                    case 'player': handle_player($method, $request[0], $input);
+                    case 'player': //handle_player($method, $request[0], $input);
                     break;
                 
                 default: header("HTTP/1.1 404 Not Found");
