a26886014dfe2433e42b93887d9740e19c9611bc
174899 <xristos_zwras@hotmail.com>
2024-01-08 11:15:08 +0200

    arxh4

--- Gkriniaris/www/js/gkriniaris.js
+++ Gkriniaris/www/js/gkriniaris.js
@@ -1,9 +1,15 @@
+$( function(){
+    drawEmptyBoard();
+    fillBoard();
+}
+);
+
+
 function drawEmptyBoard(){
     var t='<table id="gkriniarisTable">';
     for(var i=11; i>0; i--){
         t+= '<tr>';
         for(var j=1; j<12; j++){
-            
             t += '<td class="gkriniarisSquare" id="square_'+j+'_'+i+'">' + j + ','+ i +'</td>';
         }
         t += '</tr>';
@@ -12,9 +18,25 @@ function drawEmptyBoard(){
     $('#gkriniarisBoard').html(t);
 }
 
-$(drawEmptyBoard);
+function fillBoard(){
+    $.ajax(
+        {url: "gkriniaris.php/board/",
+        success: fillBoardByData
+        }
+    );
+}
 
 
 
+function fillBoardByData(data){
+    for (var i=0; i<data.length; i++){
+        var o = data[i];
+        var id = '#square_' + o.x + ' ' + o.y ;
+        var c = (o.piece!=null)?o.piece_color + o.piece: '';
+        var im = (o.piece!=null)?'<img class="piece" src="images/' +c+'.png">':''; 
+        $(id).addClass(o.b_color+'_square').html(im);
+         
+    }
+}
 
 
