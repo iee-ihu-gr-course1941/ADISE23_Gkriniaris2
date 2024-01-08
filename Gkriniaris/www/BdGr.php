7cb5053816478341de7b8d1360e96af8c1d76547
xristoskap <basterblak@gmail.com>
2024-01-08 19:57:57 +0200

    Update BdGr.php

--- Gkriniaris/www/BdGr.php
+++ Gkriniaris/www/BdGr.php
@@ -2,7 +2,7 @@
 
  require_once  "../lib/dbconnect.php";
  require_once  "../lib/board.php";
-// require_once  "../lib/game.php";
+ require_once  "../lib/game.php";
 
 
 $method = $_SERVER['REQUEST_METHOD'];
